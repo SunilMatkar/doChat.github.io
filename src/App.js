c10d6691d441231aef7f0036bf1516220f66a9a9
Sunil Matkar <98326772+SunilMatkar@users.noreply.github.com>
2022-11-11 22:11:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [countryCode, setCountryCode] = useState("+91");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const [showCountryCode, setShowCountryCode] = useState(true);
+
+  const parseNumber = (numberString) => {
+    if (numberString[0] === "+") {
+      setShowCountryCode(false);
+    } else {
+      setShowCountryCode(true);
+    }
+    return numberString.replace(/[^0-9+]/g, "");
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (phoneNumber.length > 0) {
+      window.open(
+        `https://wa.me/${
+          (showCountryCode ? countryCode.match("[0-9]+") : "") +
+          phoneNumber.replace(/[+]/g, "")
+        }`
+      );
+      setPhoneNumber("");
+      setShowCountryCode(true);
+    }
+  };
+
+  const [installEvent, setInstallEvent] = useState(null);
+  useEffect(() => {
+    const handler = (event) => {
+      event.preventDefault();
+      setInstallEvent(event);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+    return () => window.removeEventListener("beforeinstallprompt", handler);
+  }, []);
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <div>
+          {showCountryCode && (
+            <input
+              type={"tel"}
+              id="country-code-input"
+              value={countryCode}
+              size={countryCode.length}
+              maxlength="4"
+              onChange={(e) => {
+                const currChar = e.target.value[e.target.value.length - 1]
+                setCountryCode((old) => {
+                  return e.target.value.length > 0 &&
+                    (Number(currChar) || currChar === "+")
+                    ? e.target.value
+                    : old;
+                });
+              }}
+              autoComplete={false}
+            ></input>
+          )}
+          <input
+            type={"tel"}
+            id="phone-number-input"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(parseNumber(e.target.value))}
+            autoComplete={false}
+            pattern="^(\+?(\d{1,3}))?[0-9]+"
+          ></input>
+        </div>
+        <input
+          type={"submit"}
+          className={"button"}
+          disabled={!phoneNumber}
+          value={"Search"}
+        ></input>
+        {installEvent !== null && (
+          <button
+            className={"button"}
+            style={{ filter: "revert" }}
+            onClick={() => {
+              installEvent.prompt().then((event) => {
+                if (event.outcome === "accepted") {
+                  setInstallEvent(null);
+                }
+              });
+            }}
+          >
+            Install App
+          </button>
+        )}
+      </form>
+      <footer>
+        Made with ❤️ by Sunil Matkar |{" "}
+        <a
+          href={
+            "https://github.com/SunilMatkar"
+          }
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
